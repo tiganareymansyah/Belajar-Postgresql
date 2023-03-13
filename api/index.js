0fcf4a7be0c231791170270668a0c017d229f830
BangBoss25 <tiganareymansyah2502@gmail.com>
2023-03-13 21:00:34 +0700

    pertemuan 10

--- api/index.js
+++ api/index.js
@@ -8,22 +8,15 @@ app.use(express.static("public"))
 // Midlleware
 
 app.use((req, _res, next) => {
-    if(req.url === "/api/get") {
+    if(req.url === "/api/getpg") {
         console.log("Silahkan Masuk");
-    }
-    else if(req.url === "/api/getpg") {
-        console.log("Silahkan Masuk 1");
     }
     next();
 });
 
 // Route
-app.get("/api/get", async (_req, res) => {
-    res.send("Tigana Reymansyah");
-});
-
 app.get("/api/getpg", async (_req, res) => {
-    res.send((await client.query("SELECT * FROM jurusan")).rows);
+    res.send((await client.query("SELECT * FROM mahasiswa")).rows);
 });
 
 app.listen(3000, () => console.log("Server sedang berjalan..."));
